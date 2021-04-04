33a8a876edbe60d6de43aba3d7470783d7526dbc
palewire <b@palewi.re>
2021-04-03 18:22:28 -0700

    Got a poem detail page booting up dynamically based on the data files

--- baker.config.js
+++ baker.config.js
@@ -5,6 +5,8 @@ const entrypoints = [
   'poem',
 ];
 
+const fs = require('fs');
+
 export default {
   domain: 'https://cummings.ee/',
   output: process.env.BAKER_OUTPUT || '_dist',
@@ -16,8 +18,23 @@ export default {
   // use createPages to generate pages on the fly
   createPages(createPage, data) {
     const books = data.books.list;
-    for (const d of books) {
-      createPage('book_detail.html', `/book/${d.slug}`, { book: d });
+    fs.writeFile('output.json', JSON.stringify(data), 'utf8', function (err) {
+      if (err) {
+        console.log('An error occured while writing JSON Object to File.');
+        return console.log(err);
+      }
+      console.log('JSON file has been saved.');
+    });
+    for (const b of books) {
+      createPage('book_detail.html', `/book/${b.slug}/`, { book: b });
+      const poems = data.poems[b.slug] || {};
+      for (const [slug, p] of Object.entries(poems)) {
+        createPage('poem_detail.html', `/book/${b.slug}/poem/${slug}/`, {
+          book: b,
+          poem: p,
+          slug: slug,
+        });
+      }
     }
   },
 };

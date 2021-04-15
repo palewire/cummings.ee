ac454d849bebdd116f2b76d832189b8e0df98633
palewire <b@palewi.re>
2021-04-15 15:22:19 -0700

    Try again

--- baker.config.js
+++ baker.config.js
@@ -55,13 +55,13 @@ export default {
       const toc = data.toc[book.slug] || [];
       const allPoems = toc.map(getChildren).flat();
       const availablePoems = data.poems[book.slug] || {};
-      availablePoems.forEach((p) => {
+      allPoems.forEach((p) => {
         p.html_url = `https://cummings.ee/book/${book.slug}/poem/${p.slug}/`;
       });
       book.html_url = `https://cummings.ee/book/${book.slug}/`;
       book.json_url = `https://cummings.ee/book/${book.slug}.json`;
       createPage('book_detail.json.njk', `/book/${book.slug}.json`, {
-        text: JSON.stringify({ book, toc: availablePoems }, null, 2),
+        text: JSON.stringify({ book, toc: allPoems }, null, 2),
       });
       for (const [slug, poem] of Object.entries(availablePoems)) {
         poem.slug = slug;
